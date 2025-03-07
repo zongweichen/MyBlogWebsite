e09a045b10a567f2262879fd6b7b1c71951e6b05
jay <jay879944@gmail.com>
2025-03-07 15:55:12 +0800

    to store data successfully

--- BackEnd/blog/app/Http/Controllers/Api/PostController.php
+++ BackEnd/blog/app/Http/Controllers/Api/PostController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+use App\Models\Post;
+use Illuminate\Support\Facades\Validator;
+
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -20,7 +23,24 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'content' => 'required|string', // 確保內容不為空且為字串
+            'slug' => 'required|string|unique:posts,slug', // 確保 slug 唯一
+        ]);
+    
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400); // 驗證失敗，返回錯誤訊息
+        }
+    
+        // 驗證成功，儲存資料
+        $post = Post::create([
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'content' => $request->content,
+        ]);
+    
+        return response()->json($post, 201);
     }
 
     /**
